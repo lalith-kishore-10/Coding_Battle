7626e39baa4ee3c0661ef548a647568145eee66d
lalith-kishore-10 <lalithkishore.447.shs@gmail.com>
2025-10-15 09:39:11 +0530

    Last push

--- Coding_Battle/Coding_Battle/python/round2/Reverse_Line.py
+++ Coding_Battle/Coding_Battle/python/round2/Reverse_Line.py
@@ -1,1 +1,23 @@
+"""
+:)n(bif fed
+    :1>=n fi
+        n nruter
+    esle
+        )2-n(bif+)1-n(bif nruter
+))":smret fo rebmuN"(tupni(tni=m
+][=tsil_bif
+:)n(egnar ni i rof
+    ))i(bif(dneppa.tsil_bif
+)tsil_bif(tnirp
+scigol kcehc dna ylreporp noitatnedni eht egnarrA#
+"""
+
+#The user is going to reverse this by writing the program to reverse these lines.
+
+#After that the reversed lines are printed in the output, The User should cpy and paste the same in the same file and run the code retrieved from the reversed output. And that part also have some twist for the users who solve the reversing lines faster
+
+#Constraints:
+# Program to reverse each line of a given file or input
+# The program should stop getting input after the user press enter twice
+
 
